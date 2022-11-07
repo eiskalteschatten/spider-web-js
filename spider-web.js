2d823207ea093484ce53515902474d8ef401baa2
Alex Seifert <alex@alexseifert.com>
2022-11-07 14:49:12 +0100

    Draw a centered circle

--- spider-web.js
+++ spider-web.js
@@ -2,12 +2,19 @@ function drawSpiderWeb() {
   const canvas = document.getElementById('spiderWebCanvas');
 
   if (canvas) {
-    const context = canvas.getContext('2d');
-    context.beginPath();
-    context.moveTo(20, 20);
-    context.lineTo(20, 100);
-    context.lineTo(70, 100);
-    context.stroke();
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+    const ctx = canvas.getContext('2d');
+    
+    // context.beginPath();
+    // context.moveTo(20, 20);
+    // context.lineTo(20, 100);
+    // context.lineTo(70, 100);
+    // context.stroke();
+
+    ctx.fillStyle = 'Black';
+    ctx.arc(centerX, centerY, 50, 0, 2 * Math.PI);
+    ctx.fill();
   }
 }
 
