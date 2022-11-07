8a533e2102e263db07a7ed7ef84dad0346ea5432
Alex Seifert <alex@alexseifert.com>
2022-11-07 15:34:29 +0100

    Draw the main branches of the spider web

--- spider-web.js
+++ spider-web.js
@@ -1,20 +1,35 @@
 function drawSpiderWeb() {
   const canvas = document.getElementById('spiderWebCanvas');
-
+  
   if (canvas) {
-    const centerX = canvas.width / 2;
-    const centerY = canvas.height / 2;
     const ctx = canvas.getContext('2d');
     
-    // context.beginPath();
-    // context.moveTo(20, 20);
-    // context.lineTo(20, 100);
-    // context.lineTo(70, 100);
-    // context.stroke();
-
-    ctx.fillStyle = 'Black';
-    ctx.arc(centerX, centerY, 50, 0, 2 * Math.PI);
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+    
+    const twoPi = Math.PI * 2;
+    const centerCircleSize = 8;
+    
+    ctx.fillStyle = 'black';
+    ctx.arc(centerX, centerY, centerCircleSize, 0, twoPi);
     ctx.fill();
+    
+    const numberOfLegs = 8;
+    const angleOfSections = 360 / numberOfLegs;
+    
+    for (let theta = 0; theta <= 360; theta += angleOfSections) {
+      const radian = twoPi * theta / 360;
+      const x = Math.cos(radian);
+      const y = Math.sin(radian) * -1;
+
+      ctx.beginPath();
+      ctx.setTransform(1,0,0,1, centerX, centerY);
+      ctx.rotate(radian);
+      ctx.setLineDash([2, 5]);
+      ctx.moveTo(centerX, centerY);
+      ctx.lineTo(x, y);
+      ctx.stroke();
+    }
   }
 }
 
