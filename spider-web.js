7b74ebd3507e82228ce947f439721690ca6ec05f
Alex Seifert <alex@alexseifert.com>
2022-11-07 22:05:26 +0100

    Move the erro message

--- spider-web.js
+++ spider-web.js
@@ -1,74 +1,73 @@
 function drawSpiderWeb() {
   const canvas = document.getElementById('spiderWebCanvas');
   
-  if (canvas) {
-    const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    const fillColor = isDarkMode ? 'white' : 'black';
-    const twoPi = Math.PI * 2;
-    const centerX = canvas.width / 2;
-    const centerY = canvas.height / 2;
-    const distanceToEdge = Math.min(centerX, centerY)
-    const centerCircleSize = 4;
-    
-    const ctx = canvas.getContext('2d');
-        
-    ctx.arc(centerX, centerY, centerCircleSize, 0, twoPi);
-    ctx.fillStyle = fillColor;
-    ctx.fill();
-    
-    const numberOfMainBranches = 8;
-    const angleOfSections = 360 / numberOfMainBranches;
-    const lineDash = [2, 5];
+  if (!canvas) {
+    throw new Error('No canvas found!');
+  }
 
-    const getRadian = theta => twoPi * theta / 360;
+  const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const fillColor = isDarkMode ? 'white' : 'black';
+  const twoPi = Math.PI * 2;
+  const centerX = canvas.width / 2;
+  const centerY = canvas.height / 2;
+  const distanceToEdge = Math.min(centerX, centerY)
+  const centerCircleSize = 4;
+  
+  const ctx = canvas.getContext('2d');
+      
+  ctx.arc(centerX, centerY, centerCircleSize, 0, twoPi);
+  ctx.fillStyle = fillColor;
+  ctx.fill();
+  
+  const numberOfMainBranches = 8;
+  const angleOfSections = 360 / numberOfMainBranches;
+  const lineDash = [2, 5];
+
+  const getRadian = theta => twoPi * theta / 360;
+  
+  ctx.strokeStyle = fillColor;
+  ctx.setLineDash(lineDash);
+  
+  function drawMainBranches() {
+    for (let theta = 0; theta < 360; theta += angleOfSections) {
+      const radian = getRadian(theta);
+      const x = Math.cos(radian);
+      const y = Math.sin(radian);
+
+      ctx.beginPath();
+      ctx.setTransform(1,0,0,1, centerX, centerY);
+      ctx.rotate(radian);
+      ctx.moveTo(centerX, centerY);
+      ctx.lineTo(x, y);
+      ctx.stroke();
+      ctx.closePath();
+    }
+  }
+
+  function drawBetweenBranches() {
+    const radius = 80;
     
-    ctx.strokeStyle = fillColor;
-    ctx.setLineDash(lineDash);
-    
-    function drawMainBranches() {
+    for (let radiusFromOrigin = radius; radiusFromOrigin < distanceToEdge; radiusFromOrigin += radius) {
       for (let theta = 0; theta < 360; theta += angleOfSections) {
-        const radian = getRadian(theta);
-        const x = Math.cos(radian);
-        const y = Math.sin(radian);
+        const startRadian = getRadian(theta);
+        const startX = radiusFromOrigin * Math.cos(startRadian);
+        const startY = radiusFromOrigin * Math.sin(startRadian);
+        
+        const endRadian = getRadian(theta + angleOfSections);
+        const endX = radiusFromOrigin * Math.cos(endRadian);
+        const endY = radiusFromOrigin * Math.sin(endRadian);
 
         ctx.beginPath();
         ctx.setTransform(1,0,0,1, centerX, centerY);
-        ctx.rotate(radian);
-        ctx.moveTo(centerX, centerY);
-        ctx.lineTo(x, y);
+        ctx.bezierCurveTo(startX, startY, 0, 0, endX, endY);
         ctx.stroke();
         ctx.closePath();
       }
     }
-
-    function drawBetweenBranches() {
-      const radius = 80;
-      
-      for (let radiusFromOrigin = radius; radiusFromOrigin < distanceToEdge; radiusFromOrigin += radius) {
-        for (let theta = 0; theta < 360; theta += angleOfSections) {
-          const startRadian = getRadian(theta);
-          const startX = radiusFromOrigin * Math.cos(startRadian);
-          const startY = radiusFromOrigin * Math.sin(startRadian);
-          
-          const endRadian = getRadian(theta + angleOfSections);
-          const endX = radiusFromOrigin * Math.cos(endRadian);
-          const endY = radiusFromOrigin * Math.sin(endRadian);
-
-          ctx.beginPath();
-          ctx.setTransform(1,0,0,1, centerX, centerY);
-          ctx.bezierCurveTo(startX, startY, 0, 0, endX, endY);
-          ctx.stroke();
-          ctx.closePath();
-        }
-      }
-    }
-
-    drawMainBranches();
-    drawBetweenBranches();
   }
-  else {
-    console.error('No canvas found!');
-  }
+
+  drawMainBranches();
+  drawBetweenBranches();
 }
 
 window.onload = drawSpiderWeb;
