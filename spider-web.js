5429c7e94f21ca990ffdc7f8a25cc85bc53d6805
Alex Seifert <alex@alexseifert.com>
2022-11-07 19:06:50 +0100

    Plot the coordinates for where the web branches will connect

--- spider-web.js
+++ spider-web.js
@@ -2,7 +2,7 @@ function drawSpiderWeb() {
   const canvas = document.getElementById('spiderWebCanvas');
   
   if (canvas) {
-    const isDarkMode =window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
     const fillColor = isDarkMode ? 'white' : 'black';
     const twoPi = Math.PI * 2;
     const centerX = canvas.width / 2;
@@ -20,32 +20,27 @@ function drawSpiderWeb() {
     const angleOfSections = 360 / numberOfMainBranches;
     const lineDash = [2, 5];
 
-    function drawBetweenBranches(theta) {
-      const distanceBetweenBranches = 60;
-      const nextAngle = theta + angleOfSections;
+    function drawBetweenBranches() {
+      const radius = 60;
+      
+      for (let radiusFromOrigin = radius; radiusFromOrigin < distanceToEdge; radiusFromOrigin += radius) {
+        for (let theta = 0; theta < 360; theta += angleOfSections) {
+          const radian = twoPi * theta / 360;
+          const pointX = radiusFromOrigin * Math.cos(radian);
+          const pointY = radiusFromOrigin * Math.sin(radian);
 
-      for (let pointOnMainBranch = distanceBetweenBranches; pointOnMainBranch < distanceToEdge; pointOnMainBranch += distanceBetweenBranches) {
-        const startX = theta > 180 ? -pointOnMainBranch : pointOnMainBranch;
-        // y = mx + b where b is always 0 because the line always intersects the y axis at (0,0)
-        const startY = Math.tan(theta) * startX + 0;
-
-        console.log('t', theta, 'x', startX, 'y', startY)
-
-        ctx.beginPath();
-        ctx.arc(startX, startY, centerCircleSize, 0, twoPi);
-        ctx.fillStyle = 'yellow';
-        ctx.fill();
-        // ctx.setTransform(1,0,0,1, startX, startY);
-        // ctx.setLineDash(lineDash);
-        // ctx.moveTo(startX, startY);
-        // ctx.lineTo(endCoordinates, endCoordinates);
-        // ctx.strokeStyle = 'yellow';//fillColor;
-        // ctx.stroke();
+          ctx.beginPath();
+          ctx.setTransform(1,0,0,1, centerX, centerY);
+          ctx.arc(pointX, pointY, centerCircleSize, 0, twoPi);
+          ctx.fillStyle = 'yellow';
+          ctx.fill();
+          ctx.closePath();
+        }
       }
     }
 
     function drawMainBranches() {
-      for (let theta = 0; theta <= 360; theta += angleOfSections) {
+      for (let theta = 0; theta < 360; theta += angleOfSections) {
         const radian = twoPi * theta / 360;
         const x = Math.cos(radian);
         const y = Math.sin(radian) * -1;
@@ -58,12 +53,12 @@ function drawSpiderWeb() {
         ctx.lineTo(x, y);
         ctx.strokeStyle = fillColor;
         ctx.stroke();
-
-        drawBetweenBranches(theta);
+        ctx.closePath();
       }
     }
-    
+
     drawMainBranches();
+    drawBetweenBranches();
   }
   else {
     console.error('No canvas found!');
@@ -71,3 +66,4 @@ function drawSpiderWeb() {
 }
 
 window.onload = drawSpiderWeb;
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', drawSpiderWeb);
