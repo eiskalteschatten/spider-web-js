7ef028fd94a53198d05a1060d3416c480d79fcaa
Alex Seifert <alex@alexseifert.com>
2022-11-07 20:10:19 +0100

    Reorder the functions

--- spider-web.js
+++ spider-web.js
@@ -19,6 +19,24 @@ function drawSpiderWeb() {
     const numberOfMainBranches = 8;
     const angleOfSections = 360 / numberOfMainBranches;
     const lineDash = [2, 5];
+    
+    function drawMainBranches() {
+      for (let theta = 0; theta < 360; theta += angleOfSections) {
+        const radian = twoPi * theta / 360;
+        const x = Math.cos(radian);
+        const y = Math.sin(radian) * -1;
+
+        ctx.beginPath();
+        ctx.setTransform(1,0,0,1, centerX, centerY);
+        ctx.rotate(radian);
+        ctx.setLineDash(lineDash);
+        ctx.moveTo(centerX, centerY);
+        ctx.lineTo(x, y);
+        ctx.strokeStyle = fillColor;
+        ctx.stroke();
+        ctx.closePath();
+      }
+    }
 
     function drawBetweenBranches() {
       const radius = 60;
@@ -36,24 +54,6 @@ function drawSpiderWeb() {
           ctx.fill();
           ctx.closePath();
         }
-      }
-    }
-
-    function drawMainBranches() {
-      for (let theta = 0; theta < 360; theta += angleOfSections) {
-        const radian = twoPi * theta / 360;
-        const x = Math.cos(radian);
-        const y = Math.sin(radian) * -1;
-
-        ctx.beginPath();
-        ctx.setTransform(1,0,0,1, centerX, centerY);
-        ctx.rotate(radian);
-        ctx.setLineDash(lineDash);
-        ctx.moveTo(centerX, centerY);
-        ctx.lineTo(x, y);
-        ctx.strokeStyle = fillColor;
-        ctx.stroke();
-        ctx.closePath();
       }
     }
 
